f17e52e62985700eb63e25eb4ad62950c5b64264
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2021-07-18 00:16:48 -0300

    hashCode e diferença de duas contas mesmo com mesmos valores na variável

--- Dart_OrientaçãoAObjetos/alura_bank/bin/alura_bank.dart
+++ Dart_OrientaçãoAObjetos/alura_bank/bin/alura_bank.dart
@@ -1,25 +1,36 @@
 void main() {
-  ContaCorrente contaDaAmanda = ContaCorrente();
-  contaDaAmanda.titular = 'Amanda';
-  contaDaAmanda.agencia = 123;
-  contaDaAmanda.conta = 1;
+  ContaCorrente conta1 = ContaCorrente();
+  ContaCorrente conta2 = ContaCorrente();
 
-  print('Titular: ${contaDaAmanda.titular}');
-  print('Agência: ${contaDaAmanda.agencia}');
-  print('Saldo: ${contaDaAmanda.saldo}');
+  conta1.titular = 'Felipe';
+  conta1.agencia = 123;
+  conta1.conta = 1;
 
-  ContaCorrente contaDoTiago = ContaCorrente();
-  contaDoTiago.titular = 'Tiago';
-  contaDoTiago.conta = 2;
+  conta2.titular = 'Felipe';
+  conta2.agencia = 123;
+  conta2.conta = 1;
 
-  print('Titular: ${contaDoTiago.titular}');
-  print('Agência: ${contaDoTiago.agencia}');
-  print('Saldo: ${contaDoTiago.saldo}');
+  print(conta1 == conta2); //false
+  print(conta1.titular == conta2.titular); //false
+  print(conta1.agencia == conta2.agencia); //true
+  print(conta1.conta == conta2.conta); //true
+  print(conta1.saldo == conta2.saldo); //true
+
+  print(conta1.hashCode); //código hash do objeto1
+  print(conta2.hashCode); //código hash do objeto2
+
+  conta1 = conta2; //as duas contas vao ter o mesmo código hash
+  print(conta1.hashCode);
+  print(conta2.hashCode);
+  print(conta1.hashCode == conta2.hashCode);
+
+  //Referência versus Valor
+  //As duas contas tem as mesmas propriedades, mas são dois objetos diferentes um do outro
 }
 
 class ContaCorrente {
   late String titular;
-  int agencia = 145; // posso ter um valor padrão e mudar ele no objeto, exemplo disso é a agência da Amanda que foi inicializada como 123, a de Tiago não foi específica e pegou daq o valor cadastrado
+  int agencia = 145;
   late int conta;
-  double saldo = 20.0; //posso inicializar com um valor padrão que irá valer para todos os objetos
+  double saldo = 20.0;
 }
